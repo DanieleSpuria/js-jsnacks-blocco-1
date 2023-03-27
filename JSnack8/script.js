7028be2c4670f1c944909042541e6caa22573913
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-27 18:17:02 +0200

    add JSnack8

---
+++ JSnack8/script.js
@@ -0,0 +1,37 @@
+// L'utente inserisce numero a quattro cifre
+
+const num = prompt('Inserisci un numero di quattro cifre')
+const arrayNum = num.split('',);
+
+let verifica = false;
+
+
+
+// Verifico che il numero di cifre inserito sia corretto
+
+if (arrayNum.length === 4 ) {
+  verifica = true;
+} 
+
+// Dichiaro la somma, la eseguo col for e stampo risultato
+
+let sum = 0;
+
+if (verifica === true) {
+  for (let i = 0; i < 4; i++) {
+    sum += parseInt(arrayNum[i]);
+  }
+} else {
+  console.log('Il numero delle cifre inserite è sbagliato');
+}
+
+console.log(sum);
+
+
+  
+
+
+
+
+
+
